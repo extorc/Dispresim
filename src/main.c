f6b07d7d4f81a6a4dfd817f194801a29da6183d0
Shaurya Mittal <mittalshaurya004@gmail.com>
2023-02-21 19:36:12 +0530

    Rendering

--- src/main.c
+++ src/main.c
@@ -18,12 +18,25 @@ int main(void){
   glfwMakeContextCurrent(window);
 
   if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-    // std::cout << "Failed to initialize OpenGL context" << std::endl;
     return -1;
   }
 
+  float positions[6] = {
+    0.0f, 0.5f,
+    0.5f, 0.0f,
+    0.5f, 0.5f   
+  };
+
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float), 0);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
   while(!glfwWindowShouldClose(window)){
     glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glClearColor(0.7f, 0.7f, 0.8f, 1.0f);
     glfwSwapBuffers(window);
     glfwPollEvents();
