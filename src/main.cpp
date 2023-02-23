ec95812ce1049ba4da3ddab5d3852a7ec400875c
Shaurya Mittal <mittalshaurya004@gmail.com>
2023-02-23 13:07:21 +0530

    Multiple objects

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,27 @@
 #include "Square.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "vector"
 
 int main(void){
   OpenglAPI openglAPI;
   openglAPI.initializeGL(); 
 
-  Square square = loadSquare(); 
-  square.velocity = glm::vec2(rand_FloatRange(0.01f, 0.1f), rand_FloatRange(0.01f, 0.1f));
-  Square square2 = loadSquare(); 
-  square2.velocity = glm::vec2(rand_FloatRange(0.01f, 0.1f), rand_FloatRange(0.01f, 0.1f));
+  std::vector<Square> list;
+  for(int i = 0; i < 100; i++){
+    list.push_back(loadSquare()); 
+    list.at(i).velocity = glm::vec2(rand_FloatRange(0.01f, 0.1f), rand_FloatRange(0.01f, 0.1f));
+  }
+
   unsigned int program = openglAPI.bindShader();
-  SetColor(program, square.col);
+  SetColor(program, list.at(0).col);
 
   while(!glfwWindowShouldClose(openglAPI.window)){
     glClear(GL_COLOR_BUFFER_BIT);
-    collideWithWall(square); 
-    openglAPI.render(square); 
-    collideWithWall(square2); 
-    openglAPI.render(square2);
+    for(int i = 0; i < 100; i++){
+      collideWithWall(list.at(i));
+      openglAPI.render(list.at(i));
+    }
     glClearColor(0.7f, 0.7f, 0.8f, 1.0f);
     glfwSwapBuffers(openglAPI.window);
     glfwPollEvents();
