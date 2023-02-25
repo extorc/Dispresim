6cec072c6319f053e44a966aab31c3e693a26db9
Shaurya Mittal <mittalshaurya004@gmail.com>
2023-02-25 14:03:44 +0530

    Abstraction and Immunity annotation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,30 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "vector"
 
+#define IsRed(t) (true?t.col.r == 1:false)
+#define Grey Color{0.5f, 0.5f, 0.5f, 1.0f}
+int infectionCount = 1;
+
+void healPerson(std::vector<Square>& list, int i, int infectionLifetime){
+  if(list.at(i).infectionLifetime > infectionLifetime){ 
+    list.at(i).col = Grey;
+    list.at(i).infectionLifetime = 0;
+    infectionCount --;
+    list.at(i).immune = true;
+    list.at(i).col = Color{0,1,0,1};
+  }
+}
+
+void infectPerson(std::vector<Square>& list, int i, int j){
+  if(!IsRed(list.at(j)) && !list.at(j).immune){
+    float d = getDistance(list.at(j), list.at(i));
+    if(d < 5){
+      list.at(j).col = Color{1, 0, 0, 1};
+      infectionCount ++;
+    }
+  }
+}
+
 int main(int argc, char* argv[]){
   OpenglAPI openglAPI;
   openglAPI.initializeGL(); 
@@ -22,7 +46,6 @@ int main(int argc, char* argv[]){
   }
 
   unsigned int program = openglAPI.bindShader();
-  int infectionCount = 1;
 
   while(!glfwWindowShouldClose(openglAPI.window)){
     glClear(GL_COLOR_BUFFER_BIT);
@@ -31,32 +54,20 @@ int main(int argc, char* argv[]){
       openglAPI.render(list.at(i));
     }
     for(int i = 0; i < 200; i++){
-      if(list.at(i).col.r == 1){                                               //Iterate on all infected persons
+      if(IsRed(list.at(i))){                                                   //Iterate on all infected persons
         list.at(i).infectionLifetime++;
-        if(list.at(i).infectionLifetime > 2000){                               //Heal if infected for more than 2000 cycles
-          list.at(i).col = Color{0.5f, 0.5f, 0.5f, 1.0f};
-          list.at(i).infectionLifetime = 0;
-          infectionCount --;
-          list.at(i).immune = true;
-        }
+        healPerson(list, i, 1500);                                             //Heal if infected for more than x cycles
         for(int j = 0; j < 200; j++){                                          //Iterate collision with every non infected person
-          if(list.at(j).col.r != 1 && !list.at(j).immune){
-            if(j != i){
-              float d = getDistance(list.at(j), list.at(i));
-              if(d < 5){
-                list.at(j).col = Color{1, 0, 0, 1};                            //Infect
-                infectionCount ++;
-              }
-            }
-          }
+          infectPerson(list, i, j);
         }
       }
       if(list.at(i).immune){
         list.at(i).immunityLifetime++;
       }
-      if(list.at(i).immunityLifetime > 500){
+      if(list.at(i).immunityLifetime > 700){
         list.at(i).immunityLifetime = 0;
         list.at(i).immune = false;
+        list.at(i).col = Grey;
       }
     }
     std::cout<<infectionCount<<std::endl;
